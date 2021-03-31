f8f4bf0e44a60540f03fe35bb3e31fa7c6acc4b6
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-31 18:19:35 -0400

    trying various

--- installation/entrypoint.sh
+++ installation/entrypoint.sh
@@ -5,4 +5,7 @@ echo arguments are: "$@"
 echo first argument is "$1"
 evaluated_args=($(eval "echo $@"))
 echo conda run -n Template --no-capture-output ${evaluated_args[@]}
+echo $@
+all=$(echo $@)
+echo ${all}
 conda run -n Template --no-capture-output ${evaluated_args[@]}
