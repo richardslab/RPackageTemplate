e099b585cc8d63968d7feb1bd87c37b6aa9bca07
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-31 15:04:01 -0400

    use farjoun docker action

--- installation/entrypoint.sh
+++ installation/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo arguments are: "$@"
 echo first argument is "$1"
-eval evaluated_args="$@"
+eval "evaluated_args=\"$@\""
 echo conda run -n Template --no-capture-output $evaluated_args
-conda run -n Template --no-capture-output $evaluated_args
+conda run -n Template --no-capture-output ${evaluated_args}
