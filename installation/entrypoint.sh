09c5dfc5c3139e048ca8efb4e85b3417b9868097
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-31 21:44:03 -0400

    arrays...

--- installation/entrypoint.sh
+++ installation/entrypoint.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-set -eux
 
-echo arguments are: "$@"
-echo first argument is "$1"
-evaluated_args=($(eval "echo $@"))
-echo conda run -n Template --no-capture-output ${evaluated_args[@]}
-echo $@
-all=$(echo "$@")
-echo ${all[@]}
-conda run -n Template --no-capture-output ${evaluated_args[@]}
+INPUT=("$@")
+
+for index in "${!INPUT[@]}"; do  
+	printf 'input[%i] = %s\n' $index "${INPUT[$index]}";  
+done
+
+conda run -n Template --no-capture-output "${INPUT[@]}"
