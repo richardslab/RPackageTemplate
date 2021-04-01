c545e793cfd9dd5f09c9fa425fc105db7523dfea
Yossi Farjoun <farjoun@broadinstitute.org>
2021-03-31 21:08:34 -0400

    eval

--- installation/entrypoint.sh
+++ installation/entrypoint.sh
@@ -6,6 +6,6 @@ echo first argument is "$1"
 evaluated_args=($(eval "echo $@"))
 echo conda run -n Template --no-capture-output ${evaluated_args[@]}
 echo $@
-all=$(echo $@)
-echo ${all}
+all=$(echo "$@")
+echo ${all[@]}
 conda run -n Template --no-capture-output ${evaluated_args[@]}
